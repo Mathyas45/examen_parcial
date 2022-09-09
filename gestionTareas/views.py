90be07e1b640fc1d95a361a86bb6a7a06d4a2d82
USUARIO <USUARIO@DESKTOP.NONE>
2022-09-08 23:27:56 -0500

    V 2.2

--- gestionTareas/views.py
+++ gestionTareas/views.py
@@ -8,30 +8,34 @@ from django.urls import reverse
 def ingresar(request):
     #lo comente porque nose que hice mal ingeniero hice todo lo que se hizo en clase
 
-    # if request.method == 'POST':
-    #     nombre = request.POST.get('nombreUsuario')
-    #     passwordUsuario = request.POST.get('passwordUsuario')
-    #     #Validacion de informacion
-    #     usuario_registrado = 0
-    #     usuarios_totales = usuario.objects.all()
+    if request.method == 'POST':
+        nombre = request.POST.get('nombreUsuario')
+        passwordUsuario = request.POST.get('passwordUsuario')
+        #Validacion de informacion
+        usuario_registrado = 0
+        usuarios_totales = usuario.objects.all()
 
-    #     for usuario in usuarios_totales:
-    #         if usuario.nombre == nombre and usuario.psw_usuario == passwordUsuario:
-    #             usuario_registrado = 1
-        
-    #     if usuario_registrado == 1:
-    #         return HttpResponseRedirect(reverse('gestionTareas:dashboard'))
-    #     else:
-    #         return render(request,'gestionTareas/ingresar.html',{
-    #             'mensaje':'Los datos ingresados son incorrectos',
-    #         })
+        for usuario in usuarios_totales:
+            if usuario.nombre == nombre and usuario.psw_usuario == passwordUsuario:
+                usuario_registrado = 1
+    
+        if usuario_registrado == 1:
+            return HttpResponseRedirect(reverse('gestionTareas:dashboard'))
+        else:
+            return render(request,'gestionTareas/ingresar.html',{
+                'mensaje':'Los datos ingresados son incorrectos',
+            })
     return render(request,'gestionTareas/ingresar.html')
 def dashboard(request):
-    
+    # tareas_totales = tarea.objects.all()
+    # tarea = []
+    # tareas_totales = tarea.objects.filter(usuario='1')
     return render(request,'gestionTareas/dashboard.html')
+
 def creacion(request):
     return render(request,'gestionTareas/creacion.html')
 def detalle(request):
     return render(request,'gestionTareas/detalle.html')
 def edicion(request):
+    
     return render(request,'gestionTareas/edicion.html')
